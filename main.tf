1e6b3728e4f082f6856a8dc8756644b548bcfb02
unknown <nad1necookey@gmail.com>
2024-01-16 18:05:15 -0500

    adding terraform file

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.32.1"
     }
   }
@@ -14,16 +14,28 @@ provider "aws" {
 resource "aws_vpc" "vpc1" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    name ="Terraform-VPC"
+    name = "Terraform-VPC1"
   }
 }
 resource "aws_subnet" "sub1" {
-  vpc_id     = aws_vpc.vpc1.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.vpc1.id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
-  
+
   tags = {
     Name = "Terraform-sub-1"
   }
 }
-   
+
+data "aws_vpc" "default_vpc" {
+  default = true
+}
+
+resource "aws_subnet" "sub2" {
+  vpc_id            = data.aws_vpc.default_vpc.id
+  cidr_block        = "172.31.96.0/20"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "Terraform-sub-2"
+  }
+}
