8722c82ec04e4216c3050dc94d118adfc7569b18
unknown <nad1necookey@gmail.com>
2024-02-13 14:07:27 -0500

    adding vpc and subnet

---
+++ dev.tfvars
@@ -0,0 +1,6 @@
+environment       = "dev"
+vpc_cidr          = "10.0.0.0/16"
+availability_zone = "us-east-1a"
+
+
+#to apply terraform apply -var-file="dev.tfvars"
--- main.tf
+++ main.tf
@@ -7,35 +7,33 @@ terraform {
   }
 }
 
+
 provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "vpc1" {
-  cidr_block = "10.0.0.0/16"
+#the resource name explains what it is so you dont need a logical id
+variable "vpc_cider_block"  {}
+# create variable for subnet
+variable "sudnet_cidr_block" {}
+#create variable for availability zone
+variable "avail_zone" {}
+# variable for environment. you have multiple environments (such as development, staging, and production) and you want to parameterize your Terraform configurations to work seamlessly across these environments. By using an environment prefix in variables, you can create more modular and reusable Terraform code that adapts to different environments without hardcoding specific values.
+variable "env_prefix" {}
+
+# the cider number was removed and replaced with the var.vpc_cider_block
+resource "aws_vpc" "myapp_vpc" {
+  cidr_block = var.vpc_cider_block
   tags = {
-    name = "Terraform-VPC1"
+    Name = "${var.env_prefix}-vpc" #variable inside of a string. u would need the $ and {}
   }
 }
-resource "aws_subnet" "sub1" {
-  vpc_id            = aws_vpc.vpc1.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
 
+resource "aws_subnet" "mmyapp_subnet_1" {
+  vpc_id     = aws_vpc.myapp_vpc.id
+  cidr_block = var.sudnet_cidr_block
+  availability_zone = var.avail_zone
   tags = {
-    Name = "Terraform-sub-1"
-  }
-}
-
-data "aws_vpc" "default_vpc" {
-  default = true
-}
-
-resource "aws_subnet" "sub2" {
-  vpc_id            = data.aws_vpc.default_vpc.id
-  cidr_block        = "172.31.96.0/20"
-  availability_zone = "us-east-1a"
-  tags = {
-    Name = "Terraform-sub-2"
+    Name = "${var.env_prefix}-subnet-1"
   }
 }
